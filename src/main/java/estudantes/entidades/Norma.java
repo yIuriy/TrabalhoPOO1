859698b9f03f7dd8c90f0ac057d6da79c6241eb5
yIuriy <contatoiuri20@gmail.com>
2025-10-08 11:10:20 -0300

    Norma java doc

--- src/main/java/estudantes/entidades/Norma.java
+++ src/main/java/estudantes/entidades/Norma.java
@@ -4,11 +4,35 @@ import professor.entidades.CodigoCurso;
 
 import java.util.Objects;
 
-public class Norma extends DocumentoAdministrativo{
+/**
+ * Representa uma {@code Norma}, que é uma especialização de {@code DocumentoAdministrativo}.
+ *
+ * @author Iuri da Silva Fernandes
+ * @version 1.0
+ * @see DocumentoAdministrativo
+ */
+public class Norma extends DocumentoAdministrativo {
     private int numero;
     private boolean valido;
     private String texto;
 
+    /**
+     * Cria uma instância de {@code Norma}.
+     * <p>Utiliza o construtor da superclasse {@link DocumentoAdministrativo} através de {@code super()} para inicializar os atributos
+     * herdados.</p>
+     *
+     * @param criador     o criador do {@code Norma}
+     * @param codigoCurso o {@code Curso} ao qual este {@code Norma} está associado
+     * @param paginas     a quantidade de páginas
+     * @param numero      o número da {@code Norma}
+     * @param valido      se a {@code Norma} está válida
+     * @param texto       o texto da {@code Norma}
+     * @see CodigoCurso
+     * @see Documento
+     * @see DocumentoAdministrativo
+     * @since 1.0
+     *
+     */
     public Norma(String criador, CodigoCurso codigoCurso, int paginas,
                  int numero, boolean valido, String texto) {
         super(criador, codigoCurso, paginas);
@@ -17,30 +41,53 @@ public class Norma extends DocumentoAdministrativo{
         this.texto = texto;
     }
 
+    /**
+     * Retorna o número da {@code Norma}.
+     *
+     * @return o número da {@code Norma}
+     * @since 1.0
+     */
     public int getNumero() {
         return numero;
     }
 
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
+    /**
+     * Retorna se {@code Norma} está válida.
+     *
+     * @return a validade da {@code Norma}
+     * @since 1.0
+     */
     public boolean getValido() {
         return valido;
     }
 
-    public void setValido(boolean valido) {
-        this.valido = valido;
-    }
-
+    /**
+     * Retorna o texto da {@code Norma}.
+     *
+     * @return o texto da {@code Norma}
+     * @since 1.0
+     */
     public String getTexto() {
         return texto;
     }
 
-    public void setTexto(String texto) {
-        this.texto = texto;
-    }
-
+    /**
+     * Compara esta {@code Norma} com o objeto especificado para verificar igualdade.
+     *
+     * <p>A comparação considera:
+     * <ul>
+     *   <li>Os campos herdados de {@code DocumentoAdministrativo}, por meio de {@code super.equals(o)};</li>
+     *   <li>Os campos {@code numero}, {@code valido} e {@code texto} desta classe.</li>
+     * </ul>
+     *
+     * @param o o objeto a ser comparado
+     * @return {@code true} se ambos os Objetos forem instâncias de {@code Deliberacao} e possuírem os mesmo valores
+     * nos atributos herdados e nos campos {@code numero}, {@code valido} e {@code texto}; {@code false} caso contrário
+     * @see #hashCode()
+     * @see DocumentoAdministrativo#equals(Object)
+     * @see Objects#equals(Object, Object)
+     * @since 1.0
+     */
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -49,6 +96,21 @@ public class Norma extends DocumentoAdministrativo{
         return numero == norma.numero && valido == norma.valido && Objects.equals(texto, norma.texto);
     }
 
+    /**
+     * Retorna o código hash desta {@code Norma}.
+     *
+     * <p>O valor retornado é consistente com {@link #equals(Object)}, de modo que dois objetos
+     * iguais produzem o mesmo código hash.</p>
+     *
+     * <p>O valor retornado é calculado a partir do código hash da superclasse {@code super.hashCode()} combinado com
+     * os campos {@code numero}, {@code valido} e {@code texto}.</p>
+     *
+     * @return o código hash do objeto
+     * @see #equals(Object)
+     * @see Objects#hash(Object...)
+     * @see DocumentoAdministrativo#hashCode()
+     * @since 1.0
+     */
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), numero, valido, texto);
